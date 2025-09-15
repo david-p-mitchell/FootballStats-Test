c54740e25698c4b072746036fd3dd240097dabe7
David <davidmitchellfootball@googlemail.com>
2025-09-15 16:03:57 +0100

    Adding Services

---
+++ AFCStatsApp/AFCStatsApp/Services/PlayerService.cs
@@ -0,0 +1,24 @@
+﻿using AFCStatsApp.Interfaces.Repositories;
+using AFCStatsApp.Interfaces.Services;
+using AFCStatsApp.Models;
+
+namespace AFCStatsApp.Services;
+
+public class PlayerService(IPlayerRepository playerRepository) : IPlayerService
+{
+    public readonly IPlayerRepository _playerRepository = playerRepository;
+
+    public async Task<IEnumerable<PlayerModel>> GetAllAsync() => await _playerRepository.GetAllAsync();
+
+    public async Task<PlayerModel> AddAsync(PlayerModel newPlayer) => await _playerRepository.AddAsync(newPlayer);
+
+    public async Task<bool> DeleteAsync(int playerId)
+    {
+        var player = await _playerRepository.GetByIdAsync(playerId);
+        if (player == null) return false;
+        await _playerRepository.DeleteAsync(playerId);
+        return true;
+    }
+
+    public async Task<PlayerModel> UpdateAsync(PlayerModel playerToBeUpdated) => await _playerRepository.UpdateAsync(playerToBeUpdated);
+}
