31048877d382be0a27a2152a44a0b13fc6ec334a
David <davidmitchellfootball@googlemail.com>
2025-09-15 16:05:29 +0100

    add dbContext

---
+++ AFCStatsApp/AFCStatsApp/Db/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using AFCStatsApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Numerics;
+
+namespace AFCStatsApp.Db;
+
+public class AppDbContext: DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<PlayerModel> Players { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<PlayerModel>(entity =>
+        {
+            entity.HasKey(p => p.PlayerId);
+            entity.Property(p => p.PlayerName)
+                  .IsRequired()
+                  .HasMaxLength(100);
+            entity.Property(p => p.Position)
+                  .HasConversion<string>()
+                  .IsRequired()
+                  .HasMaxLength(20);
+            entity.Property(p => p.JerseyNumber)
+                  .IsRequired();
+            entity.Property(p => p.GoalsScored)
+                  .HasDefaultValue(0);
+        });
+    }
+}
