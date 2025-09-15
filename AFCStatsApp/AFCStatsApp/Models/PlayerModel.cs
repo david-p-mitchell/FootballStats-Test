2836304676fd64ee8820cef7bd14adb3ec74062a
David <davidmitchellfootball@googlemail.com>
2025-09-15 13:33:07 +0100

    add player model (and relevant enum)

---
+++ AFCStatsApp/AFCStatsApp/Models/PlayerModel.cs
@@ -0,0 +1,12 @@
+﻿namespace AFCStatsApp.Models
+{
+    public record PlayerModel
+    {
+        public int PlayerId { get; set; }
+        public required string PlayerName { get; set; }
+
+        public required PositionEnum Position { get; set; } 
+        public required short JerseyNumber { get; set; }
+        public int GoalsScored { get; set; } = 0;
+    }
+}
