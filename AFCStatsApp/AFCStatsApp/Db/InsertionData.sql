2f33c913f87d6af7abf00bc97da3c40162f910a8
David <davidmitchellfootball@googlemail.com>
2025-09-16 15:17:00 +0100

    insertion Data

---
+++ AFCStatsApp/AFCStatsApp/Db/InsertionData.sql
@@ -0,0 +1,27 @@
+INSERT INTO Players (PlayerName, Position, JerseyNumber) VALUES
+('David Raya', 'Goalkeeper', 1),
+('Kepa Arrizabalaga', 'Goalkeeper', 13),
+('William Saliba', 'Defender', 2),
+('Cristhian Mosquera', 'Defender', 3),
+('Ben White', 'Defender', 4),
+('Piero Hincapié', 'Defender', 5),
+('Gabriel Magalhães', 'Defender', 6),
+('Jurriën Timber', 'Defender', 12),
+('Riccardo Calafiori', 'Defender', 33),
+('Myles Lewis-Skelly', 'Midfielder', 49),
+('Martin Ødegaard', 'Midfielder', 8),
+('Eberechi Eze', 'Midfielder', 10),
+('Christian Nørgaard', 'Midfielder', 16),
+('Leandro Trossard', 'Midfielder', 19),
+('Noni Madueke', 'Midfielder', 20),
+('Mikel Merino', 'Midfielder', 22),
+('Declan Rice', 'Midfielder', 23),
+('Martín Zubimendi', 'Midfielder', 36),
+('Bukayo Saka', 'Midfielder', 7),
+('Gabriel Martinelli', 'Midfielder', 11),
+('Viktor Gyökeres', 'Forward', 14),
+('Kai Havertz', 'Forward', 17),
+('Leandro Trossard', 'Forward', 18);
+
+
+SELECT * FROM Players
