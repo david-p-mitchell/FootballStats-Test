bea592e448040c06a8451b8ee04ce0dd5248c5c4
David <davidmitchellfootball@googlemail.com>
2025-09-16 23:48:03 +0100

    add jquery datatables add datatable config

---
+++ AFCStatsApp/AFCStatsApp/wwwroot/js/playersTable.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+    $('#playersTable').DataTable({
+        ajax: { url: '/Players/GetAll', dataSrc: '' },
+        columns: [
+            { data: 'playerName', orderable:false },
+            { data: 'position', orderable: false },
+            { data: 'jerseyNumber', orderable: false },
+            { data: 'goalsScored' },
+            { data: null, render: (data) => `<button class="btn btn-primary edit-btn">Edit</button>`, orderable: false }
+        ],
+        paging: false,
+        scrollY: "600px",
+        scrollCollapse: true, 
+        searching: false,
+        scrollable: true,
+        ordering: true,
+        order: []
+    });
+});
