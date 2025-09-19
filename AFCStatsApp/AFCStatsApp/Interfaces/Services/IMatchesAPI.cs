48e9d52dcaf054ee0498fbeb5db48007ec8baa38
David <davidmitchellfootball@googlemail.com>
2025-09-19 04:51:53 +0100

    add MatchesAPI interface for use with refit

---
+++ AFCStatsApp/AFCStatsApp/Interfaces/Services/IMatchesAPI.cs
@@ -0,0 +1,11 @@
+﻿
+using AFCStatsApp.Models;
+using Refit;
+
+namespace AFCStatsApp.Interfaces.Services;
+
+public interface IMatchesAPI
+{
+    [Get("/teams/{teamId}/matches")]
+    Task<MatchStoreModel> GetMatchesAsync(int teamId);
+}
