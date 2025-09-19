cc61d9ab4471b423aae7774d42bf21bab0ea497d
David <davidmitchellfootball@googlemail.com>
2025-09-19 04:51:26 +0100

    adding new MatchStoreModel

---
+++ AFCStatsApp/AFCStatsApp/Models/MatchStoreModel.cs
@@ -0,0 +1,14 @@
+﻿namespace AFCStatsApp.Models;
+public record Area(int Id, string Name, string Code, string Flag);
+
+public record Team(int Id, string Name, string ShortName, string Tla, string Crest);
+
+public record Score(string Winner, string Duration, FullTimeScore FullTime, HalfTimeScore HalfTime);
+
+public record FullTimeScore(int? Home, int? Away);
+
+public record HalfTimeScore(int? Home, int? Away);
+
+public record Match(DateTime UtcDate, string Status, Area Area, Team HomeTeam, Team AwayTeam, Score Score);
+
+public record MatchStoreModel(List<Match> Matches);
