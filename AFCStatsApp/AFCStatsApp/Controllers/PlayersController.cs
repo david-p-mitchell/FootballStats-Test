f57af38e4c07dd8a11042e861099b71ed5bea95c
David <davidmitchellfootball@googlemail.com>
2025-09-19 01:45:38 +0100

    manipulate the endpoints of PlayersController add exception handling

--- AFCStatsApp/AFCStatsApp/Controllers/PlayersController.cs
+++ AFCStatsApp/AFCStatsApp/Controllers/PlayersController.cs
@@ -9,7 +9,7 @@ public class PlayersController(IPlayerService _playerService) : Controller
     public IActionResult Index() => View(); // pass the list to the view
 
 
-    [HttpGet]
+    [HttpGet("api/players/getAll")]
     public async Task<IActionResult> GetAll()
     {
         try
@@ -17,9 +17,9 @@ public class PlayersController(IPlayerService _playerService) : Controller
             var players = await _playerService.GetAllAsync();
             return Ok(players);
         }
-        catch (Exception ex)
+        catch (Exception)
         {
-            return Problem(ex.Message);
+            return Problem("Failed to get all Players");
             throw;
 
         }
@@ -29,34 +29,61 @@ public class PlayersController(IPlayerService _playerService) : Controller
     [ValidateAntiForgeryToken]
     public async Task<IActionResult> Add([FromBody]PlayerModel player)
     {
-        if (!ModelState.IsValid) return BadRequest(new { errors = "Invalid player data" });
+        if (!ModelState.IsValid || player == null) return BadRequest(new { errors = "Invalid player data" });
         if (HasPlayerId(player)) return BadRequest(new { errors = "A new player cannot have a Player Id" });
         if (!HasValidJerseyNumber(player)) return BadRequest(new { errors = "Jersey number must be 1-99" });
-        if(await _playerService.ExistsByJerseyNumberAsync(player.JerseyNumber)) return BadRequest(new { errors = "Jersey number already in use" });
+        try
+        {
+            if (await _playerService.ExistsByJerseyNumberAsync(player.JerseyNumber)) return BadRequest(new { errors = "Jersey number already in use" });
 
-        var newPlayer = await _playerService.AddAsync(player);
-        return Ok(new { success = true, player = newPlayer });
+            var newPlayer = await _playerService.AddAsync(player);
+            return Ok(new { success = true, player = newPlayer });
+        }
+        catch (Exception)
+        {
+            return Problem("Failed to add player");
+            throw;
+
+        }
     }
 
-    [HttpPost]
-    public async Task<IActionResult> Update(PlayerModel player)
+    [HttpPut("/api/players/update")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Update([FromBody] PlayerModel player)
     {
-        if (!ModelState.IsValid) return BadRequest();
-        if (!HasValidJerseyNumber(player)) return BadRequest("Jersey number must be 1-99");
-        if (await _playerService.ExistsByJerseyNumberAsync(player.JerseyNumber, player.PlayerId)) return BadRequest(new { errors = "Updated Jersey number already in use by another player." });
+        try
+        {
+            if (!ModelState.IsValid) return BadRequest();
+            if (!HasValidJerseyNumber(player)) return BadRequest("Jersey number must be 1-99");
+            if (await _playerService.ExistsByJerseyNumberAsync(player.JerseyNumber, player.PlayerId)) return BadRequest(new { errors = "Updated Jersey number already in use by another player." });
 
-        var newPlayer = await _playerService.UpdateAsync(player);
-        return Ok(newPlayer);
+            var newPlayer = await _playerService.UpdateAsync(player);
+            return Ok(new { success = true, player = newPlayer });
+        }
+        catch(Exception)
+        {
+            return Problem("Failed to update player");
+            throw;
+        }
     }
 
-    [HttpDelete("{playerid}")]
+    [HttpDelete("api/players/delete/{playerid}")]
+    [ValidateAntiForgeryToken]
     public async Task<IActionResult> Delete(int playerId)
     {
-        if (!ModelState.IsValid) return BadRequest();
-        
-        var deletedPlayer = await _playerService.DeleteAsync(playerId);
-        if (!deletedPlayer) return NotFound("Player not found");
-        return NoContent();
+        try
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            var deletedPlayer = await _playerService.DeleteAsync(playerId);
+            if (!deletedPlayer) return NotFound("Player not found");
+            return Ok(new { success = true });
+        }
+        catch (Exception)
+        {
+            return Problem("Failed to Delete Player");
+            throw;
+        }
     }
 
     private static bool HasValidJerseyNumber(PlayerModel player) => player.JerseyNumber >= 1 && player.JerseyNumber <= 99;
